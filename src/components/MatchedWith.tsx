c1be96e883b066ae0cdf84ed360612408ee42657
appimators37 <80702937+appimators37@users.noreply.github.com>
2024-11-29 21:31:55 +0500

    match color

--- src/components/MatchedWith.tsx
+++ src/components/MatchedWith.tsx
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
   },
 
   button: {
-    backgroundColor: blue_dark,
+    backgroundColor: "#1786FF",
     paddingVertical: 0,
     paddingHorizontal: 10,
     borderRadius: 25,
